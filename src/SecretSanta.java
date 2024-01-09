377a445ef709e23f488169ee3765b91f16ade8a9
popplio2 <danikbrisk@yahoo.com>
2024-01-08 23:29:30 -0500

    about to redo update function
    The previous update function was flawed (completely wrong actually) but I figured out a valid algorithm now and will rework much of the class.

--- src/SecretSanta.java
+++ src/SecretSanta.java
@@ -1,9 +1,12 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class SecretSanta {
 	public static void main(String[] args) {
@@ -11,98 +14,111 @@ public class SecretSanta {
 		players.add("Dan");
 		players.add("Alyssa");
 		players.add("Kerri");
-		players.add("Mama Simone");
-		players.add("Sir Steve");
 		
 		SecretSanta newGame = new SecretSanta(players);
 		
 		newGame.draw("Dan");
 		newGame.draw("Alyssa");
-		newGame.draw("Kerri");
-//		newGame.draw("Mama Simone");
-//		newGame.draw("Sir Steve");
 		
 		List<String> newPlayers = new ArrayList<>();
-		newPlayers.add("Nicole");
-		newPlayers.add("Silas");
-		
-//		// insufficient players test
-//		newGame.add(newPlayers);
 
-		// sufficient players
-		newPlayers.add("Youssef");
+		newPlayers.add("Mama Simone");
+		newPlayers.add("Sir Steve");
+		
 		newGame.add(newPlayers);
-		
-		
-		newGame.draw("Nicole");
-		newGame.draw("Silas");
-		newGame.draw("Youssef");
-		
-		// draw the rest
+
+		newGame.draw("Kerri");
 		newGame.draw("Mama Simone");
 		newGame.draw("Sir Steve");
+		System.out.println(newGame);
 	}
 	
 	protected Map<String, String> gameState = new HashMap<>();
 	
-	protected List<String> undrawnNames = new ArrayList<>();
-
+	protected Set<String> undrawnNames = new HashSet<>();
+	protected Set<String> hasNotDrawn = new HashSet<>();
 	
 	public SecretSanta(List<String> players) {
 		for (String player : players) {
 			this.gameState.put(player, "");
 			this.undrawnNames.add(player);
+			this.hasNotDrawn.add(player);
 		}
 		initialArrangement();
 	}
 	private void initialArrangement() {
-		Collections.shuffle(undrawnNames);
-		assignRemainingPairings();
+		List<String> undrawnNamesList = new ArrayList<>(this.undrawnNames);
+		Collections.shuffle(undrawnNamesList);
+		assignRemainingPairings(undrawnNamesList); // for the initial arrangement, undrawnNames = hasNotDrawn so it does not matter which list we arrange
 	}
 	
 	public void draw(String player) {
-		undrawnNames.remove(player);
-		
-		System.out.println(player + " is " + gameState.get(player) + "'s Secret Santa!");
+		this.undrawnNames.remove(this.gameState.get(player)); // if player was already drawn, then the list does not change
+		this.hasNotDrawn.remove(player);
+		System.out.println(player + " is " + this.gameState.get(player) + "'s Secret Santa!");
 	}
 	
 	public void add(List<String> newPlayers) {
-		if (newPlayers.size() + this.undrawnNames.size() < 3)
+		if (newPlayers.size() + this.hasNotDrawn.size() < 3)
 			System.out.println("\nInsufficient number of new players for a fair game.\n");
 		
 		else {
 			System.out.println("\nNew players added.\n");
 			
 			this.undrawnNames.addAll(newPlayers);
+			this.hasNotDrawn.addAll(newPlayers);
 			updateArrangement();
 		}
 	}
 	
 	private void updateArrangement() {
 		// erase previous pairings from undrawn names
-		for (String name : this.undrawnNames)
-			this.gameState.put(name, "");
+//		for (String name : this.hasNotDrawn)
+//			this.gameState.put(name, "");
+//		
+//		// first, (hasNotDrawn && !undrawnNames) gets to choose from undrawnNames
+//		List<String> hasNotDrawnAndNotUndrawn = new ArrayList<>(this.hasNotDrawn);
+//		for (String player : this.hasNotDrawn) { // O(n^2) - consider changing lists to hashsets for more efficient set operations
+//			for (String player2 : this.undrawnNames) {
+//				if (player.equals(player2))
+//					hasNotDrawnAndNotUndrawn.remove(player);
+//			}
+//		}
+//		for (String player : hasNotDrawnAndNotUndrawn) {
+//			String randomUndrawn = this.undrawnNames.get((int) (Math.random() * this.undrawnNames.size()));
+//			this.gameState.put(player, randomUndrawn);
+//			this.hasNotDrawn.remove(player);
+//			this.undrawnNames.remove(randomUndrawn);
+//		}
+//		
+//		// then, (hasNotDrawn && undrawnNames) choose from among each other
+//		List<String> hasNotDrawnAndUndrawn = new ArrayList<>();
+//		for (String player : this.hasNotDrawn) { // O(n^2) - consider changing lists to hashsets for more efficient set operations
+//			for (String player2 : this.undrawnNames) {
+//				if (player.equals(player2))
+//					hasNotDrawnAndUndrawn.add(player);
+//			}
+//		}
+//		Collections.shuffle(hasNotDrawnAndUndrawn);
+//		assignRemainingPairings(hasNotDrawnAndUndrawn);
 		
-		// shuffle undrawn names
-		Collections.shuffle(undrawnNames);
-		
-		// update the state 
-		assignRemainingPairings();
 	}
 	
-	private void assignRemainingPairings() { // assigning pairs based on random arrangement
-		for (int i = 0; i < undrawnNames.size() - 1; i++) {
-			gameState.put(undrawnNames.get(i), undrawnNames.get(i + 1)); 
+	private void assignRemainingPairings(List<String> players) { // assigning pairs based on random arrangement
+		if (players.size() > 2) {
+			for (int i = 0; i < players.size() - 1; i++) {
+				gameState.put(players.get(i), players.get(i + 1)); 
+			}
+			
+			// assign last pair (last element in list + first element in list)
+			gameState.put(players.get(players.size() - 1), players.get(0));
 		}
-		
-		// assign last pair (last element in list + first element in list)
-		gameState.put(undrawnNames.get(undrawnNames.size() - 1), undrawnNames.get(0));
 	}
 	
 	public String toString() {
 		String str = "";
 		for (String player : this.gameState.keySet()) {
-			str += player + ": " + gameState.get(player) + "\n";
+			str += player + ": " + this.gameState.get(player) + "\n";
 		}
 		str += "\n";
 		return str;
