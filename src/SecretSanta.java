0ae6dc0968b029703d8e23531d1b45eaec6b8c27
popplio2 <danikbrisk@yahoo.com>
2024-01-09 01:22:11 -0500

    there was no problem with random shuffling, deleting redundant import statements

--- src/SecretSanta.java
+++ src/SecretSanta.java
@@ -1,9 +1,7 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -21,7 +19,6 @@ public class SecretSanta {
 		newGame.draw("Alyssa");
 		
 		List<String> newPlayers = new ArrayList<>();
-
 		newPlayers.add("Mama Simone");
 		newPlayers.add("Sir Steve");
 		
